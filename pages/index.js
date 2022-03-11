739ef2220e59bf79c3668e37ea4b785f90f781a7
michals-src <54778121+michals-src@users.noreply.github.com>
2022-03-11 23:07:09 +0100

    add categories section

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Layout from "../src/components/layout";
 
 import Hero from "../src/sections/home/hero";
 import Newest from "../src/sections/home/newest";
+import Categories from "../src/sections/home/categories";
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
       <Layout>
         <Hero />
         <Newest />
-        <div>NEWEST ITEMS</div>
+        <Categories />
       </Layout>
     </>
   );
