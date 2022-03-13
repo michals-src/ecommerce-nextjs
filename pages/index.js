1ead6c5459dae09b3385cc6ac4ac19871f580667
michals-src <54778121+michals-src@users.noreply.github.com>
2022-03-13 12:33:18 +0100

    zmiany 12.03.2022

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,21 @@ export default function Home() {
         <Hero />
         <Newest />
         <Categories />
+        <div className="container mx-auto px-2 my-32 px-20">
+          <h1 className="text-8xl mb-16 font-bold">Czym jest CB</h1>
+          <p className="text-lg tracking-wide">Lorem ipsum dolor sit amet consectetur, adipisicing elit. At dolorum labore voluptas! Fugiat eos, aliquid repellat, consequatur pariatur maiores, sint vel non atque harum aspernatur illo sequi. Temporibus, ipsa doloremque.
+            Hic, obcaecati quidem! Tenetur, excepturi quis ut accusamus quisquam ipsam vitae esse? Ducimus distinctio ullam, velit illo tenetur dolores? Voluptatum ex porro magnam suscipit iste unde odit blanditiis, vero ducimus!
+            Provident cupiditate beatae blanditiis exercitationem? Eveniet ipsum illo nostrum vitae. Voluptas excepturi numquam ea eius distinctio eaque id beatae nisi commodi eos tempore assumenda hic, nostrum, libero nemo rem velit?</p>
+          <div className="mt-10 flex flex-row flex-wrap">
+            <div className="w-5/12 pr-20">
+              <p className="text-2xl font-bold">Korzysci z korzystania</p>
+            </div>
+            <div className="w-7/12 tracking-wide">
+              <p className="text-lg font-medium">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis quasi voluptate recusandae qui consequatur atque mollitia dicta quaerat, quod tempore, et, corrupti rerum accusamus laboriosam blanditiis deserunt tenetur tempora eos.</p>
+            </div>
+          </div>
+        </div>
+
       </Layout>
     </>
   );
--- src/sections/home/categories.js
+++ src/sections/home/categories.js
@@ -8,29 +8,36 @@ function Categories() {
           Zobacz więcej produktów.
         </h2>
         <div className='container mx-auto px-6 lg:px-2'>
-          <div className='flex flex-row flex-wrap flex-1'>
-            <div className='h-[600px] w-1/3 relative flex flex-col justify-end'>
-              <div className='w-full h-full overflow-hidden'>
-                <div className='w-full h-full overflow-hidden absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]'>
-                  <img src='https://images.unsplash.com/photo-1564023162242-91b863c6dcd4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80' />
+          <div className='flex flex-row flex-wrap flex-1 border-box mx-[-6px]'>
+            <div className='h-[600px] w-4/12 relative text-center px-6'>
+              <Link href='#' passHref>
+                <a className='absolute w-full h-full top-0 left-0'>
+                </a>
+              </Link>
+              <div className='bg-green-100 flex flex-col relative z-10'>
+                <div className="p-12">
+                  <div className='my-10'>
+                    <h6 className='font-bold text-4xl tracking-wide'>
+                      Olejki i liście
+                    </h6>
+                    <div className="mt-3">
+                      <p>Wypełnij miejsce w sprzecie</p>
+                    </div>
+                  </div>
+                  <div className='mb-10'>
+                    <Link href='#' passHref>
+                      <a className='bg-gray-700 text-white px-5 py-2 font-medium text-sm hover:bg-gray-900'>
+                        Odkryj więcej
+                      </a>
+                    </Link>
+                  </div>
                 </div>
-              </div>
-              <div className='flex flex-col p-6 relative z-10'>
-                <div className='my-10'>
-                  <h6 className='font-bold text-5xl text-white tracking-wide'>
-                    Olejki i liście
-                  </h6>
-                </div>
-                <div className='mb-10'>
-                  <Link href='#' passHref>
-                    <a className='bg-gray-600 border-2 border-gray-600 text-white px-5 py-2 rounded-full font-medium text-sm hover:bg-gray-700 hover:border-gray-700'>
-                      Odkryj więcej
-                    </a>
-                  </Link>
+                <div>
+                  <img src="https://images.unsplash.com/flagged/photo-1564833125683-b9fde4516274?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2574&q=80" />
                 </div>
               </div>
             </div>
-            <div className='h-[600px] w-1/3 relative flex flex-col justify-end'>
+            <div className='h-[600px] w-4/12 relative flex flex-col justify-start text-center px-6'>
               <div className='w-full h-full overflow-hidden'>
                 <div className='w-full h-full overflow-hidden absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]'>
                   <img src='https://images.unsplash.com/photo-1571380401583-72ca84994796?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1664&q=80' />
@@ -51,7 +58,7 @@ function Categories() {
                 </div>
               </div>
             </div>
-            <div className='h-[600px] w-1/3 relative flex flex-col justify-end'>
+            <div className='h-[600px] w-4/12 relative flex flex-col justify-start text-center px-6'>
               <div className='w-full h-full overflow-hidden'>
                 <div className='w-full h-full overflow-hidden absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]'>
                   <img src='https://images.unsplash.com/photo-1615397349754-cfa2066a298e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80' />
