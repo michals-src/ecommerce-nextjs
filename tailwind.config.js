10b1becfb6b66fb4660277a0b6cc8b8014455174
michals-src <54778121+michals-src@users.noreply.github.com>
2022-04-17 18:35:54 +0200

    Kolorystyka

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -24,6 +26,34 @@ module.exports = {
           "Helvetica Neue",
           "sans-serif",
         ],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#00B8A5",
+          100: "#E8FFFD",
+          150: "#B9FFF8",
+          200: "#5CFFEF",
+          300: "#00E6CF",
+          400: "#00CFBA",
+          500: "#00B8A5",
+          600: "#009D8E",
+          700: "#08376",
+          800: "#00766A",
+          900: "#004F47",
+        },
+
+        complement: {
+          DEFAULT: "#F7CD64",
+          100: "#FCEDC8",
+          200: "#FBE6B1",
+          300: "#FADF9B",
+          400: "#F9D885",
+          500: "#F7CD64",
+          600: "#F4BE35",
+          700: "#EBAC0C",
+          800: "#BC8A0A",
+          900: "#8D6707",
+        },
       },
     },
   },
