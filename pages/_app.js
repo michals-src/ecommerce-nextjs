9ffa6f22f1bcd0c1dd112d713190ff601193e479
michals-src <54778121+michals-src@users.noreply.github.com>
2022-05-09 22:25:50 +0200

    nprogressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import dynamic from "next/dynamic";
+
+import "nprogress/nprogress.css";
 import "../styles/globals.css";
 
+const TopProgressBar = dynamic(
+  () => {
+    return import("../src/components/SiteProgressbar");
+  },
+  { ssr: false }
+);
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <TopProgressBar />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
