181fc1d4daf11d99881cfb9b3c02e6eb2c1df34c
michals-src <54778121+michals-src@users.noreply.github.com>
2022-07-28 21:06:31 +0200

    Simple catch error && filter product data

--- pages/api/products.js
+++ pages/api/products.js
@@ -5,11 +5,11 @@
 
 //https://www.schakko.de/2020/09/05/fixing-http-401-unauthorized-when-calling-woocommerces-rest-api/
 
-import Utf8 from 'crypto-js/enc-utf8'
-import Base64 from 'crypto-js/enc-base64';
+import Utf8 from "crypto-js/enc-utf8";
+import Base64 from "crypto-js/enc-base64";
+import productFilter from "../../src/helpers/productFilter";
 
 export default async function handler(req, res) {
-
   try {
     const cred = `${process.env.STORE_CUSTOMER_KEY}:${process.env.STORE_CUSTOMER_SECRET}`;
     const hash = Base64.stringify(Utf8.parse(cred));
@@ -18,11 +18,26 @@ export default async function handler(req, res) {
       `${process.env.STORE_URL}/wp-json/wc/v3/products`,
       {
         method: "GET",
-        headers: { "Content-Type": "application/json", "Authorization": `Basic ${hash}` },
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Basic ${hash}`,
+        },
       }
     ).then(response => response.json());
 
-    res.status(200).json(data);
+    /**
+                     * todo fix
+code: "woocommerce_rest_cannot_view"
+data: {status: 401}
+message: "Sorry, you cannot list resources."
+                     */
+
+    let res_products = {};
+    if (typeof data.code === "undefined") {
+      res_products = productFilter(data);
+    }
+
+    res.status(200).json(res_products);
   } catch (e) {
     res.status(200).json({});
   }
--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -13,28 +13,38 @@ import products from "../../../fakeData/products.json";
 //   }
 // }
 
-import Utf8 from 'crypto-js/enc-utf8'
-import Base64 from 'crypto-js/enc-base64';
+import Utf8 from "crypto-js/enc-utf8";
+import Base64 from "crypto-js/enc-base64";
+import Hashids from "hashids";
+import productFilter from "../../../src/helpers/productFilter";
 
 export default async function handler(req, res) {
-
   const {
-    query: { id }
+    query: { id },
   } = req;
 
   try {
+    const hashids = new Hashids("", 8);
     const cred = `${process.env.STORE_CUSTOMER_KEY}:${process.env.STORE_CUSTOMER_SECRET}`;
     const hash = Base64.stringify(Utf8.parse(cred));
 
     const data = await fetch(
-      `${process.env.STORE_URL}/wp-json/wc/v3/products/${id}`,
+      `${process.env.STORE_URL}/wp-json/wc/v3/products/${hashids.decode(id)}`,
       {
         method: "GET",
-        headers: { "Content-Type": "application/json", "Authorization": `Basic ${hash}` },
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Basic ${hash}`,
+        },
       }
     ).then(response => response.json());
 
-    res.status(200).json(data);
+    let res_products = {};
+    if (typeof data.code === "undefined") {
+      res_products = productFilter([data])[0];
+    }
+
+    res.status(200).json(res_products);
   } catch (e) {
     res.status(200).json({});
   }
