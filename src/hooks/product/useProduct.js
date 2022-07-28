0a9aa259f05b70e3135527b744687f94ed1e8bbc
michals-src <54778121+michals-src@users.noreply.github.com>
2022-07-28 21:09:36 +0200

    read product data hook

--- src/hooks/product/useProduct.js
+++ src/hooks/product/useProduct.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import Hashids from "hashids";
+//import Hashids from "hashids";
 
 import { productsUpdate, selectProducts } from "../../slice/productsSlice";
 
-import useProductFilter from "./useProductFilter";
+//import useProductFilter from "./useProductFilter";
 
 const fetcher = url => fetch(url).then(res => res.json());
 const useProduct = slug => {
@@ -13,27 +13,27 @@ const useProduct = slug => {
 
   const [_slug, set_slug] = useState(slug);
   const [product, setProduct] = useState({});
-  const { productFilter } = useProductFilter();
+  //const { productFilter } = useProductFilter();
 
   useEffect(() => {
-    const hashids = new Hashids("", 8);
-    const id_from_slug = hashids.decode(_slug.split("-").slice(-1)[0]);
+    //const hashids = new Hashids("", 8);
+    const id_from_slug = _slug.split("-").slice(-1)[0];
+    //const id_int = hashids.decode(id_from_slug);
 
-    if (typeof _slug === "undefined" || products[_slug] === undefined) {
+    if (
+      typeof _slug === "undefined" ||
+      products.products[id_from_slug] === undefined
+    ) {
       const fetchData = async () => {
         // Fetching product from woo
         const data = await fetcher(`/api/products/${id_from_slug}`);
-        // Filtering properties of object
-        const finalData = await productFilter(data);
-        console.log(finalData);
-        setProduct(finalData);
-        const memo = {};
-        memo[id_from_slug] = finalData;
+        const memo = { [id_from_slug]: data };
         disptach(productsUpdate(memo));
+        setProduct(data);
       };
       fetchData();
     } else {
-      setProduct(products[_slug]);
+      setProduct(products.products[id_from_slug]);
     }
   }, [_slug]);
 
